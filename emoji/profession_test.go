0c605e9fc50fa7e3d0572ee60face6bf3d1ed6f6
Sam Thorogood <sam.thorogood@gmail.com>
2018-07-22 08:42:24 +1000

    profession test

---
+++ emoji/profession_test.go
@@ -0,0 +1,26 @@
+package emoji
+
+import (
+	"testing"
+)
+
+func TestProfession(t *testing.T) {
+	type testData struct {
+		in  string
+		out string
+	}
+
+	data := []testData{
+		{"👩🏻‍💻", "💻"},
+		{"🏴󠁧󠁢󠁳󠁣󠁴󠁿", ""},
+		{"👮🏿", ""},
+		{"👩🏿‍🚒", "🚒"},
+	}
+	for _, td := range data {
+		actual := ProfessionFor(td.in)
+
+		if actual != td.out {
+			t.Errorf("for %s, expected `%v` was `%v`", td.in, td.out, actual)
+		}
+	}
+}
